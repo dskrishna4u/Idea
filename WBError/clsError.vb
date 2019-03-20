5ae17f364fdd7e86ca7299d9c37fedf0b78118ae
dskrishna4u@gmail.com <dskrishna4u@gmail.com>
2019-03-20 11:51:40 +0530

    by raghavi

--- WBError/clsError.vb
+++ WBError/clsError.vb
@@ -2,7 +2,7 @@ Imports System.IO
 Imports System.Text
 Imports System.Configuration
 'new line adsfasdfasdf dgfgdgf mmmmmmmm
-
+ooooo
 Public Class clsError
     '-------------------------------------------------------------------------------------------------------------------
     'Project(s)     :       WebBIZ projects (WBError.sln)
